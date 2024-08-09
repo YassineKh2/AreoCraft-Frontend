90bd45c961e1840bf475c9eea675fdaba0a7c879
yassine <yassinkhemiri02@gmail.com>
2024-08-09 16:47:37 +0100

    Added Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
 import Page404 from "./Pages/404Page.jsx";
+import Header from "./Components/Header/Header.jsx";
 
 function App() {
 
   return (
     <>
-      <Page404 />
+      <Header />
     </>
   )
 }
