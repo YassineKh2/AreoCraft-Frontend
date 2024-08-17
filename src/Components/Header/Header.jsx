f29073f7eb2c91b7c724bc3e4b47f896922f3de6
yassine <yassinkhemiri02@gmail.com>
2024-08-17 10:54:15 +0100

    Added Stats card and dark mode

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,14 +1,17 @@
-import { Button, Navbar } from "flowbite-react";
+import {Button, DarkThemeToggle, Navbar} from "flowbite-react";
 
 export default function Header() {
     return (
         <Navbar fluid rounded className="p-5">
             <Navbar.Brand href="https://flowbite-react.com">
-                <img src="/imgs/Logo.png" className="mr-3 h-12 sm:h-16" alt="Flowbite React Logo" />
-                <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">AreoCraft</span>
+                <img src="/imgs/Logo.png" className="mr-3 h-16" alt="Flowbite React Logo" />
+                <span className="self-center hidden sm:flex whitespace-nowrap text-xl font-semibold dark:text-white">AreoCraft</span>
             </Navbar.Brand>
             <div className="flex md:order-2">
-                <Button>Get started</Button>
+                <div className="flex gap-3">
+                    <Button>Get started</Button>
+                    <DarkThemeToggle />
+                </div>
                 <Navbar.Toggle />
             </div>
             <Navbar.Collapse>
@@ -27,6 +30,7 @@ export default function Header() {
                 <Navbar.Link href="#" className="relative after:content-[''] after:absolute after:w-0 after:h-[2px] after:bottom-[-2px] after:left-0 after:bg-black after:transition-width after:duration-300 hover:after:w-full">
                     Contact
                 </Navbar.Link>
+
             </Navbar.Collapse>
         </Navbar>
     );
